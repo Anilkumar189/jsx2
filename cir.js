2291508c0639b8d36bc6c615dd0172f6aeb07cd4
Anilkumar189 <yandrapuanil143@gmail.com>
2024-09-17 16:10:23 -0400

    first commit

---
+++ cir.js
@@ -0,0 +1,194 @@
+
+var x=[
+    {
+      "firstname": "Virat",
+      "lastname": "Kohli",
+      "age": 35,
+      "gender": "Male",
+      "place": "Delhi",
+      "dob": "1988-11-05",
+      "centuries": 75
+    },
+    {
+      "firstname": "Rohit",
+      "lastname": "Sharma",
+      "age": 37,
+      "gender": "Male",
+      "place": "Bansod",
+      "dob": "1987-04-30",
+      "centuries": 44
+    },
+    {
+      "firstname": "Steve",
+      "lastname": "Smith",
+      "age": 35,
+      "gender": "Male",
+      "place": "Sydney",
+      "dob": "1989-06-02",
+      "centuries": 48
+    },
+    {
+      "firstname": "Kane",
+      "lastname": "Williamson",
+      "age": 34,
+      "gender": "Male",
+      "place": "Hudson",
+      "dob": "1990-04-08",
+      "centuries": 26
+    },
+    {
+      "firstname": "Joe",
+      "lastname": "Root",
+      "age": 33,
+      "gender": "Male",
+      "place": "Sheffield",
+      "dob": "1990-12-30",
+      "centuries": 29
+    },
+    {
+      "firstname": "Babar",
+      "lastname": "Azam",
+      "age": 29,
+      "gender": "Male",
+      "place": "Lahore",
+      "dob": "1994-10-15",
+      "centuries": 19
+    },
+    {
+      "firstname": "David",
+      "lastname": "Warner",
+      "age": 37,
+      "gender": "Male",
+      "place": "Sydney",
+      "dob": "1986-10-27",
+      "centuries": 25
+    },
+    {
+      "firstname": "AB",
+      "lastname": "de Villiers",
+      "age": 40,
+      "gender": "Male",
+      "place": "Pretoria",
+      "dob": "1984-02-17",
+      "centuries": 47
+    },
+    {
+      "firstname": "MS",
+      "lastname": "Dhoni",
+      "age": 43,
+      "gender": "Male",
+      "place": "Ranchi",
+      "dob": "1981-07-07",
+      "centuries": 10
+    },
+    {
+      "firstname": "Shakib",
+      "lastname": "Al Hasan",
+      "age": 37,
+      "gender": "Male",
+      "place": "Magura",
+      "dob": "1987-03-24",
+      "centuries": 11
+    },
+    
+    {
+      "firstname": "Pat",
+      "lastname": "Cummins",
+      "age": 31,
+      "gender": "Male",
+      "place": "Westmead",
+      "dob": "1993-05-08",
+      "centuries": 0
+    },
+    {
+      "firstname": "Jasprit",
+      "lastname": "Bumrah",
+      "age": 30,
+      "gender": "Male",
+      "place": "Gujarat",
+      "dob": "1993-12-06",
+      "centuries": 0
+    },
+    {
+      "firstname": "Yuzvendra",
+      "lastname": "Chahal",
+      "age": 34,
+      "gender": "Male",
+      "place": "Jind",
+      "dob": "1990-07-23",
+      "centuries": 0
+    },
+    {
+      "firstname": "Rashid",
+      "lastname": "Khan",
+      "age": 25,
+      "gender": "Male",
+      "place": "Jalalabad",
+      "dob": "1998-09-20",
+      "centuries": 0
+    },
+    {
+      "firstname": "Trent",
+      "lastname": "Boult",
+      "age": 35,
+      "gender": "Male",
+      "place": "Rotorua",
+      "dob": "1989-07-22",
+      "centuries": 0
+    },
+    {
+      "firstname": "Sunil",
+      "lastname": "Narine",
+      "age": 35,
+      "gender": "Male",
+      "place": "Arima",
+      "dob": "1988-05-26",
+      "centuries": 0
+    },
+    {
+      "firstname": "Mohammed",
+      "lastname": "Shami",
+      "age": 34,
+      "gender": "Male",
+      "place": "Amroha",
+      "dob": "1990-09-03",
+      "centuries": 0
+    },
+    {
+      "firstname": "Pat",
+      "lastname": "Brown",
+      "age": 32,
+      "gender": "Male",
+      "place": "Wolverhampton",
+      "dob": "1992-11-20",
+      "centuries": 0
+    },
+    {
+      "firstname": "Ricky",
+      "lastname": "Ponting",
+      "age": 49,
+      "gender": "Male",
+      "place": "Launceston",
+      "dob": "1974-12-19",
+      "centuries": 71
+    },
+    {
+      "firstname": "Brian",
+      "lastname": "Lara",
+      "age": 55,
+      "gender": "Male",
+      "place": "Santa Cruz",
+      "dob": "1969-05-02",
+      "centuries": 53
+    },
+    {
+      "firstname": "Sachin",
+      "lastname": "Tendulkar",
+      "age": 51,
+      "gender": "Male",
+      "place": "Mumbai",
+      "dob": "1973-04-24",
+      "centuries": 100
+    }
+  ]
+  
---
+++ players.js
@@ -0,0 +1,92 @@
+ var players=[
+  {
+    "fullName": "Sachin Ramesh Tendulkar",
+    "description": "Widely regarded as one of the greatest batsmen in cricket history, Tendulkar is known for his impeccable technique and consistency. He holds numerous records, including being the highest run-scorer in both Test and ODI formats.",
+    "image": "https://img.jagranjosh.com/imported/images/E/GK/sachin-records.png",
+    "centuries": {
+      "Tests": 51,
+      "ODIs": 49
+    }
+  },
+  {
+    "fullName": "Virat Kohli",
+    "description": "Known for his aggressive batting style and exceptional consistency, Kohli has been a key player for India in all formats of the game. He has served as captain of the Indian national team and has achieved significant success.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Virat_Kohli_in_PMO_New_Delhi.jpg/220px-Virat_Kohli_in_PMO_New_Delhi.jpg",
+    "centuries": {
+      "Tests": 28,
+      "ODIs": 46
+    }
+  },
+  {
+    "fullName": "Sourav Chandidas Ganguly",
+    "description": "Ganguly, often called 'Dada,' is celebrated for his leadership and aggressive batting. He played a crucial role in shaping modern Indian cricket and is known for his contributions as both a player and captain.",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlRPLiLNRzWYEwf8xMdLuaF5mu673hJ23L3g&s",
+    "centuries": {
+      "Tests": 16,
+      "ODIs": 22
+    }
+  },
+  {
+    "fullName": "Rahul Sharad Dravid",
+    "description": "Nicknamed 'The Wall' for his solid defense and technique, Dravid is renowned for his reliability and endurance in Test cricket. He was a mainstay in the Indian batting lineup for many years.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/1/17/Rahul_Dravid_in_2024.jpg",
+    "centuries": {
+      "Tests": 36,
+      "ODIs": 12
+    }
+  },
+  {
+    "fullName": "Sunil Manohar Gavaskar",
+    "description": "Gavaskar was one of the most prolific batsmen of his time and the first to reach 10,000 runs in Test cricket. His technique and concentration made him a formidable opener.",
+    "image": "https://m.media-amazon.com/images/I/91ZbXHno2UL._AC_UF1000,1000_QL80_.jpg",
+    "centuries": {
+      "Tests": 34,
+      "ODIs": 1
+    }
+  },
+  {
+    "fullName": "Kapil Dev Ramlal Nikhanj",
+    "description": "Kapil Dev, a legendary all-rounder and former captain, led India to its first World Cup victory in 1983. Known for his aggressive batting and effective medium pace bowling.",
+    "image": "https://auctions.bidandhammer.com/content/images/thumbs/0005164_kapil_dev_ramlal_nikhanj.jpeg",
+    "centuries": {
+      "Tests": 8,
+      "ODIs": 1
+    }
+  },
+  {
+    "fullName": "Anil Kumble",
+    "description": "Kumble was one of India’s greatest spin bowlers, known for his leg-spin and bounce. He achieved a historic 10-wicket haul in a Test innings against Pakistan.",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr8-kxn_F9y4p6LPWpg_sT1hA_j2BTwG4wnA&s",
+    "centuries": {
+      "Tests": 2,
+      "ODIs": 0
+    }
+  },
+  {
+    "fullName": "Mohammad Azharuddin",
+    "description": "Azharuddin was known for his stylish batting and leadership. He was a key player in the Indian team during the 1990s and captained the side in many successful series.",
+    "image": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320,q_50/lsci/db/PICTURES/CMS/319900/319944.png",
+    "centuries": {
+      "Tests": 22,
+      "ODIs": 7
+    }
+  },
+  {
+    "fullName": "Yuvraj Singh",
+    "description": "Yuvraj was an explosive all-rounder known for his powerful batting and handy left-arm spin. He played a crucial role in India’s World T20 win in 2007 and the 2011 World Cup.",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZt_0UPfHHjPcYCA5FTwyFeK9aIDlU3ZSx9w&s",
+    "centuries": {
+      "Tests": 3,
+      "ODIs": 14
+    }
+  },
+  {
+    "fullName": "Gautam Gambhir",
+    "description": "Gambhir was an aggressive opening batsman who played a crucial role in India’s victories in the 2007 World T20 and 2011 World Cup. Known for his gritty performances in high-pressure situations.",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSerpS1Jafbl2KE5oytR7UWtWAqgt40v4zohA&s",
+    "centuries": {
+      "Tests": 9,
+      "ODIs": 11
+    }
+  }
+]
